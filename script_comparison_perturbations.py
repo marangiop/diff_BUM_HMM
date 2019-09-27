b4ac26301e44f37153d5e71054bb513c56ca0f7e
marangiop <marangiopaolo@gmail.com>
2019-09-27 12:11:15 +0200

    Ran perturbation test - 58 iterations out of 150 - Xist molecule

--- script_comparison_perturbations.py
+++ script_comparison_perturbations.py
@@ -3,6 +3,7 @@ import subprocess
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+#import sys
 
 original_df = pd.read_csv('C:/Users/maran/Desktop/diff_BUM_HMM_Project/Github/diff_BUM_HMM/Xist_in vivo_vs_ex vivo_diff_BUM_HMM_analysed.txt', sep="\t", header=0)
 original_df= original_df.rename_axis('nt').reset_index()
@@ -30,7 +31,7 @@ A = pd.DataFrame({n: df.T[col].nlargest(top_n).index.tolist() for n, col in enum
 list_of_ratios = []
 iterations_counter =0
 
-for iter in range(5):
+for iter in range(150):
     print('Iteration: ' +str(iter))
     subprocess.call("Rscript --vanilla /Users/maran/Desktop/diff_BUM_HMM_Project/Github/diff_BUM_HMM/diffBUM_HMM_0.6.R", shell=True)
         
@@ -45,41 +46,43 @@ for iter in range(5):
         if check is True:
             print('Skipped this iteration')
             continue
-    
-    temporary_df=temp_df.copy()
-    temporary_df= temporary_df.rename_axis('nt').reset_index()
-    temporary_df['filteredUM'] = [0 if x<0.90 else x for x in temporary_df['UM']]
-    temporary_df['filteredMU'] = [0 if x<0.90 else x for x in temporary_df['MU']]
+        temporary_df=temp_df.copy()
+        temporary_df= temporary_df.rename_axis('nt').reset_index()
+        temporary_df['filteredUM'] = [0 if x<0.90 else x for x in temporary_df['UM']]
+        temporary_df['filteredMU'] = [0 if x<0.90 else x for x in temporary_df['MU']]
 
-    new_df= temporary_df[['filteredUM','filteredMU']]
+        new_df= temporary_df[['filteredUM','filteredMU']]
 
-    B = pd.DataFrame({n: new_df.T[col].nlargest(top_n).index.tolist() for n, col in enumerate(new_df.T)}).T
+        B = pd.DataFrame({n: new_df.T[col].nlargest(top_n).index.tolist() for n, col in enumerate(new_df.T)}).T
 
-    counter=0
-    other_counter=0
-    for i in range(len(A.index)):
-        if A.iloc[i:i+1,0:1].values[0][0] == B.iloc[i:i+1,0:1].values[0][0]:
-            counter=counter+1
-        else:
-            other_counter=other_counter+1
+        counter=0
+        other_counter=0
+        for i in range(len(A.index)):
+            if A.iloc[i:i+1,0:1].values[0][0] == B.iloc[i:i+1,0:1].values[0][0]:
+                counter=counter+1
+            else:
+                other_counter=other_counter+1
  
     
-    print(counter)
-    print(other_counter)
+        print(counter)
+        print(other_counter)
 
-    ratio = other_counter/counter
-    print(ratio)
-    list_of_ratios.append(ratio)
+        ratio = other_counter/counter
+        print(ratio)
+        list_of_ratios.append(ratio)
+        del ratio
+        del counter
+        del other_counter
+        del check
+        del new_df
 
     ending_df=temp_df.copy()
-
-    del ratio
-    del counter
-    del other_counter
-    #del B
-    #del temporary_df
-    del new_df
     del temp_df
+   
+        #del B
+        #del temporary_df
+    
+    
     iterations_counter=iterations_counter+1 
     
 
@@ -96,3 +99,6 @@ list_of_ratios_np = np.array(list_of_ratios).astype(np.float)
 print('Mean is ' + str(np.mean(list_of_ratios_np)))
 print('SD is ' + str(np.std(list_of_ratios_np)))
 print('# of actual iterations' +str(iterations_counter))
+
+
+#sys.stdout = open('file.txt', 'w')
